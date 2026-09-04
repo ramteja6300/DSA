6e2e8150871b8f7c31d6055efe96e2d76c060595
ramteja6300 <divyaveeraramtejareddyp@gmail.com>
2026-09-04 23:54:29 +0530

    Time: 8 ms (78.2%), Space: 138.3 MB (9.52%) - LeetHub

--- 3618-split-array-by-prime-indices/3618-split-array-by-prime-indices.cpp
+++ 3618-split-array-by-prime-indices/3618-split-array-by-prime-indices.cpp
@@ -1,21 +1,34 @@
-class Solution {
-public:bool isPrime(int n) {
-    if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
-}
-    long long splitArray(vector<int>& nums) {
-        int n=nums.size();
-        long long  sum1=0;
-        long long  sum2=0;
-        for(int i=0;i<n;i++){
-            if(isPrime(i))sum1+=nums[i];
-            else sum2+=nums[i];
+const int n=100001;
+vector<bool> a(n,true);
+void prime()
+{
+    a[0]=a[1]=false;
+    if(a[4]==false) return;
+    for(int i=2;i*i<=n;i++)
+    {
+        if(a[i])
+        {
+            for(int j=i*i;j<n;j=j+i)
+            {
+                if(a[j]) a[j]=false;
+            }
         }
-
-        return abs(sum1-sum2);
+    }
+}
+class Solution {
+public:
+    long long splitArray(vector<int>& nums) {
+        prime();
+        vector<int> A;
+        vector<int> B;
+        int k=nums.size();
+        for(int i=0;i<k;i++)
+        {
+            if(a[i]) A.push_back(nums[i]);
+            else B.push_back(nums[i]);
+        }
+        long long s1=accumulate(A.begin(),A.end(),0LL);
+        long long s2=accumulate(B.begin(),B.end(),0LL);
+        return abs(s1-s2);
     }
 };

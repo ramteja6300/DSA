e76ae5cf6639469e1a5e48e8aec4a25ee88609cf
ramteja6300 <divyaveeraramtejareddyp@gmail.com>
2026-09-04 22:51:04 +0530

    Time: 159 ms (96.03%), Space: 31.2 MB (81.11%) - LeetHub

---
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.cpp
@@ -0,0 +1,51 @@
+const int  n=1000001;
+vector<bool> a(n,true);
+void seive()
+{
+    a[0]=a[1]=false;
+    if(a[4]==false) return;
+    for(int i=2;i*i<=n;i++)
+    {
+        if(a[i])
+        {
+            for(int j=i*i;j<n;j=j+i)
+            {
+                if(a[j]) a[j]=false;
+            }
+        }
+    }
+
+}
+class Solution {
+public:
+    vector<vector<int>> findPrimePairs(int n) {
+        seive();
+        int l=2;
+        int r=n-2;
+        vector<vector<int>> b;
+        while(l<=r)
+         {
+            while(l <= r && !a[l]) l++;
+            while(l <= r && !a[r]) r--;
+            if(a[l] && a[r])
+            {
+                if(l+r==n)
+                {
+                    b.push_back({l,r});
+                    l++;
+                    r--;
+                }
+                else if(l+r<n)
+                {
+                    l++;
+                } 
+                else
+                {
+                    r--;
+                }   
+            }
+         }
+         return b;
+    }
+
+};
